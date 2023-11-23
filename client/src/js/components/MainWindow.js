e9e8e300cbf5d139b1c56714360bf3e5e14bdb83
tejas <upadhyaytejas100@gmail.com>
2023-11-24 02:04:05 +0530

    some styles added

--- client/src/js/components/MainWindow.js
+++ client/src/js/components/MainWindow.js
@@ -4,6 +4,39 @@ import { faPhone, faVideo } from '@fortawesome/free-solid-svg-icons';
 import ActionButton from './ActionButton';
 import { socket } from '../communication';
 
+// Include the CopyToClipboardInput component here
+function CopyToClipboardInput({ clientID }) {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopyToClipboard = () => {
+    const inputElement = document.querySelector('.txt-clientId');
+
+    if (inputElement) {
+      inputElement.select();
+      document.execCommand('copy');
+      setIsCopied(true);
+
+      // Reset the "Copied" message after a short delay
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 2000);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        className="txt-clientId"
+        defaultValue={clientID}
+        readOnly
+        onClick={handleCopyToClipboard}
+      />
+      {isCopied && <p style={{ color: 'navajowhite' }}>Copied to clipboard!</p>}
+    </div>
+  );
+}
+
 function useClientID() {
   const [clientID, setClientID] = useState('');
 
@@ -22,10 +55,6 @@ function MainWindow({ startCall }) {
   const clientID = useClientID();
   const [friendID, setFriendID] = useState(null);
 
-  /**
-   * Start the call with or without video
-   * @param {Boolean} video
-   */
   const callWithVideo = (video) => {
     const config = { audio: true, video };
     return () => friendID && startCall(true, friendID, config);
@@ -34,14 +63,11 @@ function MainWindow({ startCall }) {
   return (
     <div className="container main-window">
       <div>
+        <h1>Welcome to GBU's own video calling app!</h1>
         <h3>
-          Hi, your ID is
-          <input
-            type="text"
-            className="txt-clientId"
-            defaultValue={clientID}
-            readOnly
-          />
+          <br></br>
+          This is your ID -
+          <CopyToClipboardInput clientID={clientID} />
         </h3>
         <h4>Get started by calling a friend below</h4>
       </div>
@@ -50,7 +76,7 @@ function MainWindow({ startCall }) {
           type="text"
           className="txt-clientId"
           spellCheck={false}
-          placeholder="Your friend ID"
+          placeholder="Your friend's ID"
           onChange={(event) => setFriendID(event.target.value)}
         />
         <div>
